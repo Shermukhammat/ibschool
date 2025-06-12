bbb7216f67324ef62ae05b174bba910e2ffbb403
SHermukhammad <shermukhammadtemirov@gmail.com>
2025-06-12 21:28:55 +0500

    regex pat added

--- main/urls.py
+++ main/urls.py
@@ -1,16 +1,15 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include
+from django.urls import path, include, re_path
 from . import views
 
 
 
 urlpatterns = [
-    path('login', views.login_page, name='login_page'),
-    path('logout', views.logout_view, name='logout_page'),
-    path('help', views.help, name='help_page'),
-    path('posts/', views.PostListView.as_view(), name='posts_page'),
-    path('posts', views.PostListView.as_view(), name='posts_page'),
+    re_path('^login/?$', views.login_page, name='login_page'),
+    re_path('^logout/?$', views.logout_view, name='logout_page'),
+    re_path('^help/?$', views.help, name='help_page'),
+    re_path('^posts/?$', views.PostListView.as_view(), name='posts_page'),
     path('post/<int:year>/<int:month>/<int:day>/<slug:slug>/', views.post, name='post'),
     path('', views.main_page, name='main_page'),
 ]
